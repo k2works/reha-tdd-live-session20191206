4fd0ad6cea3641f38228ab8596fd8d19bb8d29f7
Katuyuki Kakigi <kakimomokuri@gmail.com>
2019-12-06 17:46:13 +0900

    test: 1から100まで数を返す

--- test/fizz_buzz.test.js
+++ test/fizz_buzz.test.js
@@ -26,9 +26,37 @@ suite("FizzBuzzTest", () => {
       assert.equal("1", fizzbuzz.convert(1));
     });
   });
+  suite("1から100まで数を返す", () => {
+    test("はじめは1を返す", () => {
+      const fizzbuzz = new Fizzbuzz();
+      const result = fizzbuzz.print1To100();
+      assert.equal(1, result[0]);
+    });
+    test("最後は文字列Buzzを返す", () => {
+      const fizzbuzz = new Fizzbuzz();
+      const result = fizzbuzz.print1To100();
+      assert.equal("Buzz", result[99]);
+    });
+    test("3の場合は文字列Fizzを返す", () => {
+      const fizzbuzz = new Fizzbuzz();
+      const result = fizzbuzz.print1To100();
+      assert.equal("Fizz", result[2]);
+    });
+    test("5の場合は文字列Buzzを返す", () => {
+      const fizzbuzz = new Fizzbuzz();
+      const result = fizzbuzz.print1To100();
+      assert.equal("Buzz", result[4]);
+    });
+    test("15の場合は文字列FizzBuzzを返す", () => {
+      const fizzbuzz = new Fizzbuzz();
+      const result = fizzbuzz.print1To100();
+      assert.equal("FizzBuzz", result[14]);
+    });
+  });
 });
 
 class Fizzbuzz {
+  constructor() {}
   convert(number) {
     let result = number;
     if (result % 3 === 0 && result % 5 === 0) {
@@ -39,5 +67,13 @@ class Fizzbuzz {
       result = "Buzz";
     }
     return result.toString();
+  }
+
+  print1To100() {
+    let result = [];
+    for (let i = 0; i <= 100; i++) {
+      result[i] = this.convert(i + 1);
+    }
+    return result;
   }
 }
