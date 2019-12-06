b83f6ba786ff263a455455b2955c7502522af282
Katuyuki Kakigi <kakimomokuri@gmail.com>
2019-12-06 17:50:50 +0900

    refactor: 関数宣言の変更

--- test/fizz_buzz.test.js
+++ test/fizz_buzz.test.js
@@ -29,27 +29,27 @@ suite("FizzBuzzTest", () => {
   suite("1から100まで数を返す", () => {
     test("はじめは1を返す", () => {
       const fizzbuzz = new Fizzbuzz();
-      const result = fizzbuzz.print1To100();
+      const result = fizzbuzz.generateList();
       assert.equal(1, result[0]);
     });
     test("最後は文字列Buzzを返す", () => {
       const fizzbuzz = new Fizzbuzz();
-      const result = fizzbuzz.print1To100();
+      const result = fizzbuzz.generateList();
       assert.equal("Buzz", result[99]);
     });
     test("3の場合は文字列Fizzを返す", () => {
       const fizzbuzz = new Fizzbuzz();
-      const result = fizzbuzz.print1To100();
+      const result = fizzbuzz.generateList();
       assert.equal("Fizz", result[2]);
     });
     test("5の場合は文字列Buzzを返す", () => {
       const fizzbuzz = new Fizzbuzz();
-      const result = fizzbuzz.print1To100();
+      const result = fizzbuzz.generateList();
       assert.equal("Buzz", result[4]);
     });
     test("15の場合は文字列FizzBuzzを返す", () => {
       const fizzbuzz = new Fizzbuzz();
-      const result = fizzbuzz.print1To100();
+      const result = fizzbuzz.generateList();
       assert.equal("FizzBuzz", result[14]);
     });
   });
@@ -63,7 +63,7 @@ class Fizzbuzz {
     return number.toString();
   }
 
-  print1To100() {
+  generateList() {
     let result = [];
     for (let i = 0; i <= 100; i++) {
       result[i] = this.convert(i + 1);
